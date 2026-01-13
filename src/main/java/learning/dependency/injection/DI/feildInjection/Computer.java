9641c64e4b9fa1121a73b5bab7fc59614bb1f036
luoyonglun1 <vengroth3322@gmail.com>
2026-01-13 20:12:51 +0700

    git commit

---
+++ src/main/java/learning/dependency/injection/DI/feildInjection/Computer.java
@@ -0,0 +1,5 @@
+package learning.dependency.injection.DI.feildInjection;
+
+public interface Computer {
+    void compile() ;
+}
--- src/main/java/learning/dependency/injection/DI/feildInjection/Dev.java
+++ src/main/java/learning/dependency/injection/DI/feildInjection/Dev.java
@@ -1,12 +1,25 @@
 package learning.dependency.injection.DI.feildInjection;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
 
+    // to connect to each other we use @Autosired here and @Component at Laptop class
+    // if not the laptop default is null ;
+    // we avoid create object like this Laptop laptop = new Laptop ;
+    @Autowired // this is call feildInjection
+    Computer comp ;
+
+
+
     public void build(){
+        // @Component is at Laptop file it will show the Laptop.compile because laptop is implements from Computer
+        // if there are @Component on both Desktop and Laptop file it will be error cuz it only want once not twice or more
+        // if both haave @Component you cam give it priority by at @Primary to give it priority
+        comp .compile();
         System.out.println("I building spring boot project");
     }
 
